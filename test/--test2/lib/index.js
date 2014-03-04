42caa518d7bd71bfc4752f4287a97a44cb60db4c
Marcus Stong <stongo@gmail.com>
2014-03-03 20:33:53 -0500

    might need to delete it

---
+++ test/--test2/lib/index.js
@@ -0,0 +1,17 @@
+// Declare internals
+
+var internals = {};
+
+
+// Plugin registration
+
+exports.register = function (plugin, options, next) {
+
+    var err = null;
+
+    if (err) throw err;
+
+    return next();
+};
+
+
